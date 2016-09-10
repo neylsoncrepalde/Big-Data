b4685e9dde0ea3eb806d4d42ecb4be90f20c742b
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-09-10 01:57:24 -0300

    Update mining_univ_mineiras.py

--- Python/mining_univ_mineiras.py
+++ Python/mining_univ_mineiras.py
@@ -22,5 +22,24 @@ def pegalinks(url):
     print('Lista de links pronta!')
     return links_validos
 
-links = pegalinks('http://www.altillo.com/pt/universidades/brasil/estado/minasgerais.asp')
-print(links)
+paginas = pegalinks('http://www.altillo.com/pt/universidades/brasil/estado/minasgerais.asp')
+paginas_completas = []
+for pagina in paginas:
+    item = ('http://www.altillo.com/pt/universidades/brasil/estado/'+ pagina)
+    paginas_completas.append(item)
+print(paginas_completas)
+
+tipoTag = type(bsObj.blockquote)
+#Entrando em todas as paginas
+for pagina in paginas_completas[:2]:
+    try:    
+        html = urlopen(pagina)
+    except HTTPError as e:
+        print('Page not found')
+    bsObj = BeautifulSoup(html.read(), 'lxml')
+    cursos = []
+    for child in bsObj.find('blockquote').descendants:
+        if type(child) == tipoTag:
+            print(child.get_text())
+        else:
+            print(child)
