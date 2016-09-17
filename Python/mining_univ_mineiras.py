802afc6a152106627ff5a44fa26b067105336e61
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-09-17 00:57:41 -0300

    Update mining_univ_mineiras.py

--- Python/mining_univ_mineiras.py
+++ Python/mining_univ_mineiras.py
@@ -3,6 +3,8 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import os
+import csv
 
 def pegalinks(url):
     try:    
@@ -29,17 +31,32 @@ for pagina in paginas:
     paginas_completas.append(item)
 print(paginas_completas)
 
-tipoTag = type(bsObj.blockquote)
+def tipoTag(bsObj):
+    return type(bsObj.blockquote)
+    
+def pega_nome(link):
+    nome = link[66:]
+    inst = nome[:-4]
+    return inst.replace("_"," ")
+    
+os.chdir('/home/neylson/Documentos/Neylson Crepalde/Doutorado/GIARS/Antonio')
+saida = open('universidades.csv', 'w')
+export = csv.writer(saida, quoting=csv.QUOTE_NONNUMERIC)
+
 #Entrando em todas as paginas
-for pagina in paginas_completas[:2]:
+for pagina in paginas_completas:
     try:    
         html = urlopen(pagina)
     except HTTPError as e:
         print('Page not found')
     bsObj = BeautifulSoup(html.read(), 'lxml')
     cursos = []
-    for child in bsObj.find('blockquote').descendants:
-        if type(child) == tipoTag:
+    for child in bsObj.find('blockquote').findAll('p'):
+        if type(child) == tipoTag(bsObj):
+            print(pega_nome(pagina))
             print(child.get_text())
+            export.writerow([pega_nome(pagina), child.get_text()])
         else:
+            print(pega_nome(pagina))
             print(child)
+            export.writerow([pega_nome(pagina), child])
