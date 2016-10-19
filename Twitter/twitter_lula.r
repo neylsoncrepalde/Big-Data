9d74d1f1b3351a21849ab7b9a7ad71ffad9b7185
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-10-19 00:39:46 -0200

    Update twitter_lula.r

--- Twitter/twitter_lula.r
+++ Twitter/twitter_lula.r
@@ -10,10 +10,10 @@ library(plyr)
 #necessary file for Windows
 #download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
 
-consumer_key <- "zZqautC9KVJXXcNAWVtOp0ipZ"
-consumer_secret <- "QYccdNmLoLpSje7wFgY2GXo1qXofj4qIxK6fJPTrw1xHKqd5ou"
-access_token <- "3036885015-RwYnJZAvbTnL6OBbkzgN3QGDPlhN5WtDQBp8ZdL"
-access_secret <- "8duOLfdbJqbHem69FHzJD63hKEBCLDv0mesPLiB4v4dJ3"
+consumer_key <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+consumer_secret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_secret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 setup_twitter_oauth(consumer_key,
                     consumer_secret,
