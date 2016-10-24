2b7efea3741523e7cd4dae073fd859f5b06cf22c
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-10-24 01:34:10 -0200

    Update analise_rede_migracoes.R

--- Migrations/analise_rede_migracoes.R
+++ Migrations/analise_rede_migracoes.R
@@ -10,6 +10,8 @@ library(igraph)
 library(intergraph)
 library(dplyr)
 library(magrittr)
+library(ggplot2)
+library(NetworkRiskMeasures)
 
 setwd("~/Documentos/Neylson Crepalde/Doutorado/sea_populacoes/Seminario Big Data/")
 
@@ -43,6 +45,7 @@ V(g.ll)$lon = latlon$lon
 
 # plotando a rede
 indeg = igraph::degree(g.ll, mode = "in")
+outdeg = igraph::degree(g.ll, mode="out")
 coords = tk_coords(1)
 
 png("migration_network.png", width = 1920, height = 1080)
@@ -54,9 +57,37 @@ plot(g.ll, vertex.size=indeg/40, edge.arrow.size=.3, edge.curved=T,
 title(main = "Migration Flows", xlab="1980-2013\nSize = Indegree")
 dev.off()
 
-simple = simplify(g.ll)
-n = asNetwork(simple)
+#simple = simplify(g.ll)
+#n = asNetwork(simple)
 
+####################
+# Algumas análises
+g.ll
+graph.density(g.ll)
+diameter(g.ll, directed = T, unconnected=F)
+inter = igraph::betweenness(g.ll, weights = NA, normalized = T)
+head(sort(inter, decreasing = T))
+const = constraint(g.ll)
+head(sort(const))
+head(sort(indeg, decreasing = T))
+
+h1 = ggplot(NULL, aes(indeg))+geom_histogram(fill='#ec7063')+labs(x='Indegree', y='')+theme_bw()
+h2 = ggplot(NULL, aes(outdeg))+geom_histogram(fill='#5dade2')+labs(x='Outdegree', y='')+theme_bw()
+h3 = ggplot(NULL, aes(const))+geom_histogram(fill='#58d68d')+labs(x='Constraint', y='')+theme_bw()
+multiplot(h1,h2,h3, cols = 3)
+triad_census(g.ll)
+
+tipos = c("003","012","102","021D","021U","021C","111D","111U","030T","030C",
+          "201","120D","120U","120C","210","300")
+xtable::xtable(cbind(tipos, triad_census(g.ll)))
+transitivity(g.ll, type = "global") # medida para redes undirected
+reciprocity(g.ll)
+
+# tentando investigar medida de contágio
+#contagio = contagion(g.ll, weights = weight ,exposure_type = "vulnerability", 
+#                     buffer = matrix(1, ncol = 224, nrow=224), method = "debtrank", shock = "all")
+#contagio$simulations
+#summary(contagio)
 
 # Plotando a rede no mapa
 
@@ -152,8 +183,9 @@ dev.off()
 
 # Plotando com IGRAPH
 
-png("migration_black_igraph.png", width = 1920, height = 1080)
+pdf("migration_black_igraph_small.pdf", width = 14, height = 8)
 map("world", col="#191919", fill=TRUE, bg="#000000", lwd=0.08)
+#map("world", col="#f2f2f2", fill=TRUE, bg="white", lwd=0.04)
 # plot the network using the geo coordinates
 plot(g.ll,  # pass in the network
      # don't erase the map before drawing the network
@@ -166,7 +198,8 @@ plot(g.ll,  # pass in the network
      layout=cbind(V(g.ll)$lon,V(g.ll)$lat),  
      # ---- all the rest of these are optional to make it look nice ------
      # set a semi-transparent edge color
-     edge.color=adjustcolor("#5F9EA0", .05),
+     edge.color=adjustcolor("#5F9EA0", .03),
+     #edge.color=adjustcolor('#AA555555', .15),
      edge.width=3,
      # making edges curve
      edge.curved=T,
@@ -178,6 +211,6 @@ plot(g.ll,  # pass in the network
      vertex.color='white',
      vertex.frame.color='white')
 
-title(main="Migration flows", col.main=adjustcolor("white", .7))
+#title(main="Migration flows", col.main=adjustcolor("white", .7))
 dev.off()
 
