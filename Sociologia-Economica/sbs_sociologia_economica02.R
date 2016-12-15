4bd67355a04166cf9022491aca39b816ed326157
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-12-15 20:32:36 -0200

    Create sbs_sociologia_economica02.R

---
+++ Sociologia-Economica/sbs_sociologia_economica02.R
@@ -0,0 +1,14 @@
+# SBS
+# vendo as keywords
+
+library(readr)
+keys.df = read_csv('~/Documentos/Neylson Crepalde/Doutorado/GIARS/SBS_sociologia_economica/keywords_df.csv')
+
+library(descr)
+library(magrittr)
+
+keys = keys.df$keyword %>% unique
+
+selecao = grep("(e|E)con(ô|o)m", keys.df$keyword)
+
+View(keys.df[selecao,])
