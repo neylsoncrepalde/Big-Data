e140b355fe1373fb18efa369476642f19b8e1e7e
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-12-16 15:41:10 -0200

    Update sbs_sociologia_economica02.R

--- Sociologia-Economica/sbs_sociologia_economica02.R
+++ Sociologia-Economica/sbs_sociologia_economica02.R
@@ -16,19 +16,3 @@ selecao = grep("(e|E)con(ô|o)m", keys.df$keyword)
 
 soc.econ = keys.df[selecao,]
 #write.table(soc.econ, 'artigos_soc_econ.csv', sep=';', row.names = F)
-
-# Medidas descritivas
-keys = soc.econ$keyword %>% tolower
-keys = keys %>% gsub('(à|á|ã)','a',.) %>% gsub('(é|ê|è)','e',.) %>% gsub('í','i',.) %>%
-  gsub('(ó|ö|ò|ô|õ)','o',.) %>% gsub('(ú|ù|ü)','u',.)
-
-library(wordcloud)
-pal = brewer.pal(8,"Dark2")
-wordcloud(keys, min.freq = 2, random.order = F, colors = pal)
-
-tabela.key = table(keys) %>% as.data.frame(., stringsAsFactors=F)
-tabela.key = arrange(tabela.key, desc(Freq))
-
-library(ggplot2)
-ggplot(tabela.key[1:10,], aes(reorder(keys, Freq), Freq) )+geom_bar(stat = "identity")+
-  coord_flip()+labs(y="",x="")+theme_bw()
