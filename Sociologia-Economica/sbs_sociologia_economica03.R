9eac1264ce0bf288e01dbb1dd3c48b8d4aeade7c
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-12-16 15:41:38 -0200

    Update sbs_sociologia_economica03.R

--- Sociologia-Economica/sbs_sociologia_economica03.R
+++ Sociologia-Economica/sbs_sociologia_economica03.R
@@ -8,9 +8,11 @@ library(magrittr)
 library(dplyr)
 library(descr)
 library(igraph)
+library(wordcloud)
+library(ggplot2)
 
-title =   read_csv('title.csv')
-keys =    read_csv('keys.csv')
+title =   read_csv('title.csv') %>% unique
+keys =    read_csv('keys.csv') %>% unique
 autores = read_csv('autores.csv')
 refs =    read_csv('refs.csv')
 
@@ -31,8 +33,25 @@ plot(coautorias, vertex.size=6, vertex.label=NA)
 
 
 #################################
+#Freq das palavras chave
+# Medidas descritivas
+keys.vec = keys$keyword %>% tolower
+keys.vec = keys.vec %>% gsub('(à|á|ã)','a',.) %>% gsub('(é|ê|è)','e',.) %>% gsub('í','i',.) %>%
+  gsub('(ó|ö|ò|ô|õ)','o',.) %>% gsub('(ú|ù|ü)','u',.)
+
+pal = brewer.pal(8,"Dark2")
+wordcloud(keys.vec, min.freq = 5, random.order = F, colors = pal)
+
+tabela.key = table(keys.vec) %>% as.data.frame(., stringsAsFactors=F)
+tabela.key = arrange(tabela.key, desc(Freq))
+
+ggplot(tabela.key[1:10,], aes(reorder(keys.vec, Freq), Freq) )+geom_bar(stat = "identity")+
+  coord_flip()+labs(y="",x="")+theme_bw()
+
+## Juntando os bancos para montar a rede
 data2 = full_join(data1, keys)
 View(data2)
+
 # Rede de palavras-chave
 m2 = data2[,c(1,5)] %>% as.matrix
 rede2mode.keys = graph_from_edgelist(m2, directed = F)
@@ -48,7 +67,7 @@ data3 = full_join(data1, refs)
 View(data3)
 # Rede de citações
 data3 = mutate(data3,
-       refsemponto = tm::removePunctuation(ref))
+               refsemponto = tm::removePunctuation(ref))
 
 m3 = data3[,c(4,6)] %>% as.matrix
 citacoes = graph_from_edgelist(m3, directed=T)
