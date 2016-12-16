6cb55856ccf66d7cd62fa680f598435a39f25a74
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-12-16 19:46:54 -0200

    Update sbs_sociologia_economica03.R

--- Sociologia-Economica/sbs_sociologia_economica03.R
+++ Sociologia-Economica/sbs_sociologia_economica03.R
@@ -46,7 +46,7 @@ ggplot(tabela.autor[1:21,], aes(x=reorder(Var1, Freq), y=Freq))+geom_bar(stat = 
 
 # Rede de coautorias
 data1 = full_join(title, autores)
-View(data1)
+#View(data1)
 
 m1 = data1[,c(1,4)] %>% as.matrix
 rede2mode.autores = graph_from_edgelist(m1, directed = F)
@@ -74,7 +74,7 @@ ggplot(tabela.key[1:10,], aes(reorder(keys.vec, Freq), Freq) )+geom_bar(stat = "
 
 ## Juntando os bancos para montar a rede
 data2 = full_join(data1, keys)
-View(data2)
+#View(data2)
 
 # Rede de palavras-chave
 data2$keyword = data2$keyword %>% tolower
@@ -95,12 +95,54 @@ plot(palavras.chave, vertex.label.cex=degree(palavras.chave)/60,
 title(xlab="Layout = Escalonamento Multidimensional")
 
 ################################
+# Medidas descritivas das citações
 data3 = full_join(data1, refs)
-View(data3)
-# Rede de citações
+#View(data3)
+
 data3 = mutate(data3,
                refsemponto = tm::removePunctuation(ref))
+#limpando alguns
+#sel.bourdieu = grep("(B|b)(o|O)(u|U)(r|R)(d|D)(i|I)(e|E)(u|U) (p|P)",data3$refsemponto)
+#data3$refsemponto[sel.bourdieu] = "BOURDIEU Pierre"
+#sel.bourdieu2 = grep("(B|b)(o|O)(u|U)(r|R)(d|D)(i|I)(e|E)(u|U)",data3$refsemponto)
+#jerome = grep("Jer", data3$refsemponto[sel.bourdieu2])
+#data3$refsemponto[sel.bourdieu2][-jerome] = "BOURDIEU Pierre"
 
+#sel.swedberg = grep("S(w|W)(e|E)(d|D)(b|B)(e|E)(r|R)(g|G)",data3$refsemponto)
+#data3$refsemponto[sel.swedberg] = "SWEDBERG Richard"
+#sel.granovetter = grep("G(r|R)(a|A)(n|N)(o|O)(v|V)(e|E)(t|T)(t|T)(e|E)(r|R)",data3$refsemponto)
+#ellen = grep("Ellen", data3$refsemponto)
+#sel.granovetter = sel.granovetter[-which(sel.granovetter %in% ellen == T)]
+#data3$refsemponto[sel.granovetter] = "GRANOVETTER Mark"
+
+#sel.sen = grep("S(e|E)(n|N) A",data3$refsemponto)
+#data3$refsemponto[data3$refsemponto=="SEN A"] = "SEN Amartya"
+#data3$refsemponto[data3$refsemponto=="SEN A K"] = "SEN Amartya"
+#data3$refsemponto[data3$refsemponto=="Sen A"] = "SEN Amartya"
+#data3$refsemponto[data3$refsemponto=="Sen Amartya"] = "SEN Amartya"
+
+#sel.piola = grep("P(i|I)(o|O)(l|L)(a|A)",data3$refsemponto)
+#data3$refsemponto[sel.piola] = "PIOLA Sérgio"
+
+#sel.weber = grep("W(e|E)(b|B)(e|E)(r|R)",data3$refsemponto)
+#florence = grep("W(e|E)(b|B)(e|E)(r|R) F",data3$refsemponto[sel.weber])
+#data3$refsemponto[sel.weber][-florence][-19] = "WEBER Max"
+
+#sel.marx = grep("M(a|A)(r|R)(x|X)", data3$refsemponto)
+#data3$refsemponto[sel.marx][-c(18,25)] = "MARX Karl"
+
+#sel.singer = grep("S(i|I)(n|N)(g|G)(e|E)(r|R)", data3$refsemponto)
+#singer.outros = grep("S(i|I)(n|N)(g|G)(e|E)(r|R) (A|M|H)",data3$refsemponto[sel.singer])
+#data3$refsemponto[sel.singer][-singer.outros][-1] = "SINGER Paul"
+
+data3 = mutate(data3,
+               refsemponto = tolower(refsemponto))
+
+tabela.ref = table(data3$refsemponto) %>% as.data.frame(., stringsAsFactors=F)
+tabela.ref = arrange(tabela.ref, desc(Freq))
+tabela.ref[1:50,]
+
+# Rede de citações
 m3 = data3[,c(4,6)] %>% as.matrix
 citacoes = graph_from_edgelist(m3, directed=T)
 indeg = degree(citacoes, mode = "in")
