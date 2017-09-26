320f6dd27475dcd710fa8ecb78a78c78ca951213
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-09-25 22:33:05 -0300

    major update

--- Facebook/Bolsonaro-teste/analise_conteudo.R
+++ Facebook/Bolsonaro-teste/analise_conteudo.R
@@ -3,6 +3,10 @@
 # Neylson Crepalde
 #########################
 
+# Se precisar instalar os pacotes:
+#install.packages(c("data.table","tm","wordcloud","magrittr"), 
+#                 dependencies = T)
+
 # Carregando pacotes necessários
 library(data.table)
 library(tm)
@@ -17,7 +21,7 @@ setwd('~/raspagem_face')
 list.files()
 
 #lê os dados
-coments = fread("page_211857482296579_2017_09_25_23_02_02_comments.tab",
+coments = fread("https://raw.githubusercontent.com/neylsoncrepalde/Big-Data/master/Facebook/Bolsonaro-teste/page_211857482296579_2017_09_25_23_02_02_comments.tab",
                 encoding="UTF-8")
 
 #Exibe os primeiros casos dos comentários
@@ -25,11 +29,13 @@ head(coments$comment_message)
 
 #Limpa os dados
 
-conteudo = coments$comment_message %>% removePunctuation %>%
+conteudo = coments$comment_message %>% tolower %>% removePunctuation %>%
   removeWords(., stopwords('pt'))
 
 # Monta a nuvem de palavras
-wordcloud(enc2native(conteudo), min.freq=4, max.words = 100, random.order = F)
+pal2 = brewer.pal(8, "Dark2")
+wordcloud(enc2native(conteudo), min.freq=4, max.words = 100, random.order = T, 
+          colors = pal2)
 
 #----------------------
 # Análise de conteúdo
@@ -43,8 +49,4 @@ df.scale <- scale(df)
 d <- dist(df.scale, method = "euclidean")
 fit.ward2 <- hclust(d, method = "ward.D2")
 plot(fit.ward2)
-
-
-
-
 
