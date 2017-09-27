8612735f4aeb1f093f9d6abddfcc88256c5fc3c0
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-09-27 10:41:49 -0300

    Update criticas.R

--- OSB/criticas.R
+++ OSB/criticas.R
@@ -23,10 +23,10 @@ library(wordcloud)
 library(tm)
 library(plyr)
 
-consumer_key <- "zZqautC9KVJXXcNAWVtOp0ipZ"
-consumer_secret <- "QYccdNmLoLpSje7wFgY2GXo1qXofj4qIxK6fJPTrw1xHKqd5ou"
-access_token <- "3036885015-RwYnJZAvbTnL6OBbkzgN3QGDPlhN5WtDQBp8ZdL"
-access_secret <- "8duOLfdbJqbHem69FHzJD63hKEBCLDv0mesPLiB4v4dJ3"
+consumer_key <- "XXXXXXXXXXXXXXXXXX"
+consumer_secret <- "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_secret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 setup_twitter_oauth(consumer_key,
                     consumer_secret,
