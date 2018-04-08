c5f8e0bc8ad0d2c956c524bc3f9e7ded1e1c02a0
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-04-08 15:41:09 -0300

    incompleto numero de colunas diferentes

---
+++ Twitter/processa_json_prisao_lula_parallel.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 10:54:03 2018
+
+@author: neylson
+"""
+
+import json
+from json import JSONDecodeError
+import pandas as pd
+import time
+import sys
+from multiprocessing import Pool
+
+t1 = time.time()
+
+file = open(str(sys.argv[1]), 'r')
+#file = open('prisao_lula_tweets3.txt', 'r')
+
+jsonFile = file.readlines()
+
+ex = json.loads(jsonFile[0])
+bdex = pd.DataFrame.from_dict(ex)
+bdex['user_name'] = bdex['user']['screen_name']
+bdex['user_favourites_count'] = bdex['user']['favourites_count']
+bdex['user_followers_count'] = bdex['user']['followers_count']
+bdex['user_id'] = bdex['user']['id']
+bdex['user_id_str'] = bdex['user']['id_str']
+
+print('Processando arquivo: ' + str(sys.argv[1]))
+print('Quantidade de tweets a serem processados: ' + str(len(jsonFile)))
+print('\n')
+time.sleep(2)
+    
+tweets = bdex.loc[['user_mentions']]
+nomes = [nome for nome in tweets.columns]
+
+def processaJson(file):
+    global tweets
+    global nomes
+    global bd
+    try:
+        l = json.loads(file)
+    except JSONDecodeError:
+        pass
+    try:
+        bd = pd.DataFrame.from_dict(l)
+    except ValueError:
+        pass
+    try:
+        bd['user_name'] = bd['user']['screen_name']
+    except KeyError:
+        pass
+    bd['user_favourites_count'] = bd['user']['favourites_count']
+    bd['user_followers_count'] = bd['user']['followers_count']
+    bd['user_id'] = bd['user']['id']
+    bd['user_id_str'] = bd['user']['id_str']
+    bd1 = bd.loc[nomes]
+    
+    tweets = pd.concat([tweets, bd1.loc[['user_mentions']]], 
+                       ignore_index = True)
+    
+    return tweets
+
+print('Definindo threads...')
+p = Pool(4)
+
+print('Processando...')
+res = p.map(processaJson, jsonFile[:100])
+
+
+
+#print('Número de tweets com erro: ' + str(len(erros)))
+print('Dimensões do banco de tweets coletado: ' + str(tweets.shape))
+
+print('Exportando...')
+
+tweets.to_csv(str(sys.argv[1])[:-3] + 'csv')
+
+t2 = time.time()
+
+print('Pronto!')
+print('Tempo de execução: ' + str(t2 - t1) + ' segundos.')
